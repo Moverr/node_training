ef4a4c612dd14cd018c91dab699955d533dc0584
Muyinda Rogers <moverr@gmail.com>
2019-12-07 18:10:18 +0300

    Server js, file :

--- server.js
+++ server.js
@@ -3,5 +3,8 @@ const hostname = '127.0.0.1';
 const port = 9978
 
 http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-type':'text/plain'})
-})
+    res.writeHead(200,{'Content-type':'text/plain'});
+    res.end('Hello World \n');
+}).listen(port,hostname,()=>{
+    console.log('Server running at httl://${hostname}:${port}')
+});
