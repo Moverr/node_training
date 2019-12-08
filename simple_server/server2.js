2b24d29fdc502bb4180fb45914909d3bea92833d
Muyinda Rogers <moverr@gmail.com>
2019-12-08 10:17:54 +0300

    :Debugging :

--- simple_server/server2.js
+++ simple_server/server2.js
@@ -1,13 +1,18 @@
 const http = require("http")
 const url = require("url")
-const path  = require("path")
+const path = require("path")
 const fs = require("fs")
 
 
 const mimeTypes = {
-    "html":"text/html",
-    "jpeg":"image/jpeg",
-    "jpg":"image/jpg",
-    "png":"image/png",
-    "js":"text/javascript" 
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpg",
+    "png": "image/png",
+    "js": "text/javascript"
 }
+
+http.createServer((req, res) => {
+    var uri = url.parse(req, url).pathname;
+    var fileName = path.join(process.cwd(),unescape(uri))
+})
